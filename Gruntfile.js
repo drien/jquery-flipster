6939114ad72a2396fab87cdd363579be69cad4d2
Stephen Shaw <shshaw@gmail.com>
2015-05-20 15:08:09 -0500

    Switched to LESS, adjusted Gruntfile, added uncompressed CSS output to allow users to adjust styles as they need.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,52 @@
 module.exports = function(grunt) {
 
+  var globalConfig = {
+        today: grunt.template.today("yyyy-mm-dd"),
+        banner: '/*! <%= pkg.name %>, built <%= globalConfig.today %> */\n',
+        autoprefix: new (require('less-plugin-autoprefix'))({browsers: ["last 3 versions", 'ie >= 9']}),
+      };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    globalConfig: globalConfig,
+
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %>, built <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: globalConfig.banner,
       },
       dist: {
         files: {
-          'dist/jquery.flipster.min.js': ['src/js/jquery.flipster.js']
+          'dist/jquery.flipster.min.js': ['src/jquery.flipster.js']
         }
       }
     },
-    cssmin: {
-      add_banner: {
+    less: {
+      full: {
         options: {
-            banner: '/*! <%= pkg.name %>, built <%= grunt.template.today("yyyy-mm-dd") %> */'
+          banner: globalConfig.banner,
+          plugins: globalConfig.autoprefix
         },
         files: {
-          'dist/jquery.flipster.min.css': ['src/css/jquery.flipster.css']
+          'dist/jquery.flipster.css': ['src/less/jquery.flipster.less', 'src/less/*/*.less']
+        }
+      },
+      minified: {
+        options: {
+          banner: globalConfig.banner,
+          plugins: globalConfig.autoprefix,
+          compress: true
+        },
+        files: {
+          'dist/jquery.flipster.min.css': ['src/less/jquery.flipster.less', 'src/less/*/*.less']
         }
       }
-    },
-    autoprefixer: {
-        dist: {
-            src: 'dist/jquery.flipster.min.css'
-        }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
-  grunt.registerTask('default', ['uglify', 'cssmin', 'autoprefixer']);
+  grunt.registerTask('default', ['uglify', 'less']);
 
 };
